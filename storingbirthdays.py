bde09ec535c9d257148aa7d915dad7d220942c90
irenepeety <irene.peetiya@gmail.com>
2025-11-03 18:47:35 +0700

    Add files via upload

---
+++ storingbirthdays.py
@@ -0,0 +1,16 @@
+irene=27
+iwin=15
+father=8
+mother=13
+print("Irene's birthday date is",irene)
+print("iwin's birthday date is",iwin)
+print("father's birthday date is",father)
+print("mother's birthday date is",mother)
+irene="may"
+iwin="january"
+father="october"
+mother="february"
+print("irene's birthday month is",irene)
+print("iwin's birthday month is",iwin)
+print("father's birthday month is",father)
+print("mother's birthday month is",mother)
